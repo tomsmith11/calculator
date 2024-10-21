4381f34d008fd7e3882dbf6adb84f2525a1d7e19
tomsmith <103900032+tomsmith11@users.noreply.github.com>
2024-10-21 12:01:41 +0100

    Further update script.js [WIP]
    Still a huge WIP, but it knows when buttons are pressed however operators do not currently work

--- script.js
+++ script.js
@@ -1,40 +1,73 @@
 document.addEventListener('DOMContentLoaded', () => {
     const calculator = document.querySelector('.calculator');
     const keys = calculator.querySelector('.calculator__keys');
-
-    keys.addEventListener('click', e => {
-        if (e.target.matches('button')) {
-          // 
-        }
-
-        const key = e.target
-        const action = key.dataset.action
-
+    const display = document.querySelector('.calculator__display');
+  
+    keys.addEventListener('click', (e) => {
+      if (e.target.matches('button')) {
+        const key = e.target;
+        const action = key.dataset.action;
+        const keyContent = key.textContent;
+        const displayedNum = display.textContent;
+  
         if (!action) {
-            console.log('number key!');
+          if (displayedNum === '0') {
+            display.textContent = keyContent;
+          } else {
+            display.textContent = displayedNum + keyContent;
+          }
         }
-
+  
+        if (action === 'decimal') {
+          display.textContent = displayedNum + '.';
+        }
+  
+        if (action === 'clear') {
+          display.textContent = '0';
+        }
+  
+        if (action === 'calculate') {
+          console.log('equal key!');
+        }
+  
         if (
+          action === 'add' ||
+          action === 'subtract' ||
+          action === 'multiply' ||
+          action === 'divide'
+        ) {
+          console.log('operator key!');
+        }
+      } 
+      keys.addEventListener('click', e => {
+        if (e.target.matches('button')) {
+          // ...
+      
+          if (
             action === 'add' ||
             action === 'subtract' ||
             action === 'multiply' ||
             action === 'divide'
-        ) {
-            console.log('operator key!');
+          ) {
+            key.classList.add('is-depressed')
+            // Add custom attribute
+            calculator.dataset.previousKeyType = 'operator'
+          }
         }
+      })
+      const previousKeyType = calculator.dataset.previousKeyType
 
-        if (action === 'decimal') {
-            console.log('decimal key!')
-          }
-          
-          if (action === 'clear') {
-            console.log('clear key!')
-          }
-          
-          if (action === 'calculate') {
-            console.log('equal key!')
-          }
-          
+      if (!action) {
+        if (displayedNum === '0' || previousKeyType === 'operator') {
+          display.textContent = keyContent
+        } else {
+          display.textContent = displayedNum + keyContent
+        }
+      }
 
-    })
+      if (action === 'calculate') {
+        const secondValue = displayedNum
+        // ...
+      }
+    });
   });
