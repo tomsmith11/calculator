a873d28b9a8ad0b665aeed1ac1d4d6277d304ea0
tomsmith <103900032+tomsmith11@users.noreply.github.com>
2024-10-21 10:38:35 +0100

    Update script.js
    Adds js to console.log when different types of keys are pressed (decimal, clear, equals, numbers and operators

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
-const calculator = document.querySelector('.calculator')
-const keys = calculator.querySelectorAll('button')
+document.addEventListener('DOMContentLoaded', () => {
+    const calculator = document.querySelector('.calculator');
+    const keys = calculator.querySelector('.calculator__keys');
 
+    keys.addEventListener('click', e => {
+        if (e.target.matches('button')) {
+          console.log('button clicked');
+        }
+
+        const key = e.target
+        const action = key.dataset.action
+
+        if (!action) {
+            console.log('number key!');
+        }
+
+        if (
+            action === 'add' ||
+            action === 'subtract' ||
+            action === 'multiply' ||
+            action === 'divide'
+        ) {
+            console.log('operator key!');
+        }
+
+        if (action === 'decimal') {
+            console.log('decimal key!')
+          }
+          
+          if (action === 'clear') {
+            console.log('clear key!')
+          }
+          
+          if (action === 'calculate') {
+            console.log('equal key!')
+          }
+          
+
+    })
+  });
